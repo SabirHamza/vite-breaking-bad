f739e5f29ab8bdfc2792d3526f321c6debb4b1bb
SabirHamza <h.sabir.ma@gmail.com>
2023-03-01 17:20:34 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import HeaderApp from './components/HeaderApp.vue'
+
+export default {
+  components: {
+    HeaderApp
+  }
+}
+</script>
+
+<template>
+  <HeaderApp />
+</template>
+
+<style scoped></style>
